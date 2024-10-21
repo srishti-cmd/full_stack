635cb337ac553a64ed4a7f36a789336867943c71
srishti-cmd <srishtiagl142@gmail.com>
2024-10-21 12:43:47 +0530

    assesment

---
+++ js_work/index.js
@@ -0,0 +1,39 @@
+// console.log("hello world");
+// var a=12
+// console.log("a="+a);
+// if(a>10)
+// {
+//     a=30;
+//     console.log("hiiii",a);
+// }
+
+// let a=12;
+// let b="10";
+// let s=Symbol("122");
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof s);
+// function sum(a,b,c=10)
+// {
+// return a+b+c
+// }
+// let total=sum(23,45);
+// console.log(total);
+// function ccompiler()
+// {
+//     return " c compiler selected";
+// }
+// function javacompiler()
+// {
+//     return " java compiler selected";
+// }
+// function selectlang(lang)
+// {
+//     if (lang=="c")
+//         console.log(ccompiler());
+//     else if (lang=="java")
+//         console.log(javacompiler());
+// }
+// selectlang("java");
+
+console.log("hii");
